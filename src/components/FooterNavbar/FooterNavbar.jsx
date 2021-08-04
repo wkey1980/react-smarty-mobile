0c4363b24cb86854556db2b4c7ab9675ddda041e
Will <28814921+wkey1980@users.noreply.github.com>
2021-08-04 23:31:34 +0100

    Starting to build the footer section

---
+++ src/components/FooterNavbar/FooterNavbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const FooterNavbar = () => {
+	return (
+		<div>
+			<nav>
+				<ul>
+					<li><a href="#">Terms and conditions</a></li>
+					<li><a href="#">Privacy policy (PDF)</a></li>
+					<li><a href="#">Accessibility</a></li>
+					<li><a href="#">Vulnerable situations policy (PDF)</a></li>
+					<li><a href="#">Cookies policy (PDF)</a></li>
+					<li><a href="#"></a>Complaints</li>
+				</ul>
+
+				<ul>
+					<li><a href="#">SIM only</a></li>
+					<li><a href="#">Pay as you go</a></li>
+					<li><a href="#">Data SIM</a></li>
+					<li><a href="#">No credit check</a></li>
+					<li><a href="#">Unlimited SIM</a></li>
+					<li><a href="#">Student SIM</a></li>
+					<li><a href="#">Data Rollover</a></li>
+					<li><a href="#">5G</a></li>
+				</ul>
+
+				<ul>
+					<li><a href="#">Support</a></li>
+					<li><a href="#">Contact us</a></li>
+					<li><a href="#">App</a></li>
+					<li><a href="#">Price guide (PDF)</a></li>
+					<li><a href="#">Roaming & International</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">three.co.uk</a></li>
+				</ul>
+
+				<ul>
+					<li><a href="#">Activate SIM</a></li>
+					<li><a href="#">Apple - App Store</a></li>
+					<li><a href="#">GooglePlay</a></li>
+				</ul>
+			</nav>
+		</div>
+	)
+}
+
+export default FooterNavbar
+
+
+
+
+
+
+
+
