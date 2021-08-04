a28f1e4baf8cb91b80ab8c5be32de7c3b105630c
Will <28814921+wkey1980@users.noreply.github.com>
2021-08-04 23:14:51 +0100

    Started to build the Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from '../Navbar/index'
+
+const Header = () => {
+	return (
+		<div>
+			<h1>Header Component</h1>
+			<Navbar />
+		</div>
+	)
+}
+
+export default Header
