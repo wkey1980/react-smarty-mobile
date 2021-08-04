d236b96e2361d0f4e8703052efcba7586412a6fb
Will <28814921+wkey1980@users.noreply.github.com>
2021-08-04 23:15:38 +0100

    Update App.jsx and add logo to project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,16 @@
+// Import Material UI Components
+import { CssBaseline } from '@material-ui/core';
+
 import './styles/App.Styles.css';
+
+// Import App Components
+import Header from '../Header/index'
 
 function App() {
 	return (
 		<div className="app">
+			<CssBaseline />
+			<Header />
 			<h1>React App - Smarty Mobile</h1>
 		</div>
 	);
